43b086a7cb97467b4278dc8589ae0f6b2ab3e64b
Zoltan Haindrich <kirk@rxd.hu>
2019-03-20 10:16:04 +0100

    redirect stdout/err to log

--- src/main/java/kevin/CmdExecutor.java
+++ src/main/java/kevin/CmdExecutor.java
@@ -1,17 +1,46 @@
 
 package kevin;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CmdExecutor {
+
+  static class StreamGobbler implements Runnable {
+    private InputStream inputStream;
+    private Consumer<String> consumeInputLine;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumeInputLine) {
+      this.inputStream = inputStream;
+      this.consumeInputLine = consumeInputLine;
+    }
+
+    @Override
+    public void run() {
+      new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumeInputLine);
+    }
+  }
 
   public static int executeCommandLine(final String[] commandLine, final long timeout)
       throws IOException, InterruptedException, TimeoutException {
 
-    ProcessBuilder processBuilder = new ProcessBuilder(commandLine).inheritIO();
+    Logger logger = LoggerFactory.getLogger(CmdExecutor.class);
+
+    ProcessBuilder processBuilder = new ProcessBuilder(commandLine);
     processBuilder.environment().putAll(Settings.instance().getExecEnvironment());
     Process process = processBuilder.start();
+    StreamGobbler outputGobbler = new StreamGobbler(process.getInputStream(), logger::info);
+    StreamGobbler errorGobbler = new StreamGobbler(process.getErrorStream(), logger::error);
+
+    new Thread(outputGobbler).start();
+    new Thread(errorGobbler).start();
     Worker worker = new Worker(process);
     worker.start();
     try {
