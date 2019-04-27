3cad34351b39fdcd25d352ece95c1c6eaffc97f4
Zoltan Haindrich <kirk@rxd.hu>
2019-04-27 14:52:56 +0200

    add prometheus

---
+++ src/main/java/kevin/PromMetricsServer.java
@@ -0,0 +1,24 @@
+
+package kevin;
+
+import java.io.IOException;
+
+import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.HTTPServer;
+
+public class PromMetricsServer {
+  public static final Counter loops =
+      Counter.build().name("loops_total").help("Total number of main loops.").register();
+  final HTTPServer server;
+
+  public PromMetricsServer(int port) throws IOException {
+    server = new HTTPServer(port, true);
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    PromMetricsServer ps = new PromMetricsServer(1234);
+    Thread.sleep(10000);
+
+  }
+
+}
