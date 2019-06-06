a414e330c372bddb2effc9856ed76daf9019bf6c
Zoltan Haindrich <kirk@rxd.hu>
2019-06-06 16:51:29 +0200

    de-lombokize

--- src/main/java/hu/rxd/kevin/slack/SlackMessage.java
+++ src/main/java/hu/rxd/kevin/slack/SlackMessage.java
@@ -1,17 +1,32 @@
 
 package hu.rxd.kevin.slack;
 
-import lombok.*;
-
 import java.io.Serializable;
 
-@AllArgsConstructor
-@Builder(builderClassName = "Builder")
-@Getter
-@Setter
+//@AllArgsConstructor
+//@Builder(builderClassName = "Builder")
+//@Getter
+//@Setter
 public class SlackMessage implements Serializable {
 
   private String username;
   private String text;
   private String icon_emoji;
+
+  static class Builder {
+    SlackMessage instance;
+
+    public Builder text(String string) {
+      instance.text = string;
+      return this;
+    }
+
+    public SlackMessage build() {
+      return instance;
+    }
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
 }
